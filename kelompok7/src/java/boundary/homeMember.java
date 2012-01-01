f5b6706ca0064cf1356a46b3fe5ba2efb132aa7d
pen.tall.goreng <pen.tall.goreng@gmail.com>
2012-01-01 12:25:56 +0000

    issue 7

---
+++ kelompok7/src/java/boundary/homeMember.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boundary;
+
+import entity.DaftarLapangan;
+import entity.lapangan;
+import entity.operator;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class homeMember extends Boundary {
+    private operator operator;
+
+    public homeMember() {
+        super();
+        setTemplate("/WEB-INF/homeMember.jsp");
+    }
+
+    @Override
+    protected void process() {
+      /*setMessage("");
+        DaftarLapangan dl = new DaftarLapangan();
+        if(validate_lapangan()){
+            List<DaftarLapangan> getLapangan = (List<DaftarLapangan>) dl.getLapangan(operator);
+            getRequest().setAttribute("daftarLapangan", getLapangan.iterator());
+        /*}else{
+            try {
+                getResponse().sendRedirect("homeMember");
+            } catch (IOException ex) {
+                Logger.getLogger(homeMember.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private boolean validate_lapangan(){
+        DaftarLapangan dl = new DaftarLapangan();
+        int xx = dl.getJumlahLapangan();
+        
+        if( xx == 0){
+            return false;
+        }
+
+        return true;
+    }  */
+    }
+}
