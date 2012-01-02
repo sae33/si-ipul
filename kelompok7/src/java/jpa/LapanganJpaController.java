842dae153c471c8c0e3225d308d7a0015354d6fa
yogi.agra.w <yogi.agra.w@gmail.com>
2012-01-02 13:16:21 +0000

    menambah template editBerita.jsp, activity.jsp, homeMember.jsp menambah jpa/InfoJpaController.java, dan jpa/LapanganJpaController.java

---
+++ kelompok7/src/java/jpa/LapanganJpaController.java
@@ -0,0 +1,129 @@
+package jpa;
+
+import entity.lapangan;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import jpa.exceptions.NonexistentEntityException;
+
+public class LapanganJpaController {
+
+    public LapanganJpaController() {
+        emf = Persistence.createEntityManagerFactory("kelompok7PU");
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(lapangan lap) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(lap);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(lapangan lap) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            lap = em.merge(lap);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = lap.getId();
+                if (findLapangan(id) == null) {
+                    throw new NonexistentEntityException("Lapangan dengan id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            lapangan lap;
+            try {
+                lap = em.getReference(lapangan.class, id);
+                lap.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("Lapangan dengan id " + id + " no longer exists.", enfe);
+            }
+            em.remove(lap);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<lapangan> findLapanganEntities() {
+        return findLapanganEntities(true, -1, -1);
+    }
+
+    public List<lapangan> findLapanganEntities(int maxResults, int firstResult) {
+        return findLapanganEntities(false, maxResults, firstResult);
+    }
+
+    private List<lapangan> findLapanganEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(lapangan.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public lapangan findLapangan(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(lapangan.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getLapanganCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<lapangan> rt = cq.from(lapangan.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+}
---
+++ kelompok7/web/WEB-INF/activity.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : homeMember
+    Created on : 16 Nov 11, 23:48:13
+    Author     : yogi
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Hello Member</h1>
+        <br>
+        <a href ="logout">Logout</a>
+        <a href ="editProfil">Edit Profil</a>
+        <a href ="activity">Activity</a>
+        <a href ="listLapangan">Daftar Lapangan</a>
+    </body>
+</html>
