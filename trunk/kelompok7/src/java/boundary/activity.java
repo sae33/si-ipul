6231ca0b948865a766a182ce7ffc1f24c69f70e0
sam.pambudi@gmail.com <sam.pambudi@gmail.com@35855de2-00b1-624c-f68b-9148395879d9>
2012-01-02 15:48:40 +0000

    menambahkan boundary activity

---
+++ trunk/kelompok7/src/java/boundary/activity.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boundary;
+
+import entity.DaftarLapangan;
+import entity.DaftarUser;
+import entity.lapangan;
+import entity.members;
+import entity.operator;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class activity extends Boundary {
+    private operator operator;
+
+    public activity() {
+        super();
+        setTemplate("/WEB-INF/activity.jsp");
+    }
+
+    @Override
+    protected void process() {
+      setMessage("");
+        DaftarUser dl = new DaftarUser();
+        if(validate_activity()){
+            List<lapangan> i = dl.lapanganTerbaru();
+            getRequest().setAttribute("daftar_lapangan", i.iterator());
+        }
+        else{
+            try {
+                getResponse().sendRedirect("listLapanganKosong");
+            } catch (IOException ex) {
+                Logger.getLogger(activity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            }
+        }
+
+    private boolean validate_activity(){
+        DaftarUser dl = new DaftarUser();
+        int xx = dl.getJumlahLapangan();
+        
+        if( xx == 0){
+            return false;
+        }
+
+        return true;
+    }  
+}
+
